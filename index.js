8febad587569ece9e4c15ce97fa82206acacf3b3
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-06-30 23:16:26 +0600

    delete api

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ async function run() {
 
 
 
+        // delete an order 
+        app.delete('/delete-billing/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await collection.deleteOne(query)
+            res.json(result)
+        })
+
+
     } finally {
         // await client.close();
     }
