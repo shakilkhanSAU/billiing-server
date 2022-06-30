5caa674a623e239d059873d1a0047325b0f395cd
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-06-30 23:14:47 +0600

    post api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 
@@ -7,11 +8,31 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://doctors-portal:3ZLbk0GRoV8GMacK@cluster0.yw3x3.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("billingDatabase");
+        const collection = database.collection("billing");
+
+        // add a new bill  (POST API)
+        app.post('/add-billing', async (req, res) => {
+            const bill = req.body;
+            const result = await productsCollection.insertOne(bill);
+            res.json(result)
+        })
+
+
+
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
